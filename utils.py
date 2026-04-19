d8206cc02dd1b06fc021abdff9599c97e0d4ea0e
Lelouch <panicadd@gmail.com>
2026-04-19 20:16:27 +0400

    fix: remove duplicates in badges

--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ def deep_merge(master, new_data):
     for key, value in new_data.items():
         if value is None:
             continue
-        
+
         if key in master:
             if isinstance(value, list) and isinstance(master[key], list):
-                master[key].extend(value)
+                master[key] = list({item: None for item in (master[key] + value)}.keys())
             elif isinstance(value, dict) and isinstance(master[key], dict):
                 deep_merge(master[key], value)
             else:
